ca6ed396c069e51f19e6fa0eda7aae7e3a785e7a
alisonysy <qwen735@aucklanduni.ac.nz>
2020-08-19 23:30:15 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function(){
+  function initCountdown(){
+    let timespan = countdown(new Date(2020, 4, 23), null, countdown.YEARS | countdown.MONTHS | countdown.DAYS, 4).toString();
+    const el = document.getElementById('countdown');
+    el.append(timespan);
+  };
+  initCountdown();
+  function initPopup(){
+    $.magnificPopup.open({
+      removalDelay: 300,
+      items: {
+        src: '#letter'
+      },
+      type: 'inline',
+      mainClass: 'mfp-fade',
+      showCloseBtn: true,
+      closeBtnInside: true,
+      callbacks: {
+        afterClose: function() {
+          console.log('Popup is completely closed',this);
+
+        },
+      }
+    });
+  };
+  $(document).ready(function(){
+    $('.avatars').on('click', initPopup);
+  })
+})();
